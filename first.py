b3ab25ebe8c09cf22f128a1256a66df37950e644
epsilonot <roshidulalom2017@gmail.com>
2024-03-29 15:21:49 +0600

    python

---
+++ first.py
@@ -0,0 +1,3 @@
+print("I have a car")
+import keyword
+print(keyword.kwlist)
---
+++ py_practice.py
@@ -0,0 +1,35 @@
+import math as gfg
+print(gfg.factorial(5))
+print()
+g=lambda x: x*x*x
+print(g(7))
+
+print("i have a boy")
+print("You should do it")
+import math
+from math import factorial
+print(factorial(5))
+print(math.factorial(5))
+
+print()
+print()
+
+
+a=10
+b=0
+try:
+    k=a//b
+    print(k)
+except ZeroDivisionError:
+    print("Cannot divided by ZeroDivisionError")
+
+
+
+finally:
+    print(a)
+    print(b)
+    del a
+    del b
+    print(a)
+    print(b)
+

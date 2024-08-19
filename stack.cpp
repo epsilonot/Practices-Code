c9fa7fd2afc424000a50a096bfac5e9ca3320ea3
epsilonot <roshidulalom2017@gmail.com>
2024-08-19 15:28:32 +0600

    fk

--- stack.cpp
+++ stack.cpp
@@ -1,15 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    stack<int>bou;
-    bou.push(1);
-    bou.push(2);
-    bou.push(3);
-    bou.push(4);
-    bou.push(5);
-    bou.push(6);
-while(!bou.empty()){
-    cout<<bou.top()<<" ";
-    bou.pop();
+    stack<int>pstu;
+    pstu.push(1);
+    pstu.push(2);
+    pstu.push(3);
+    pstu.push(4);
+    pstu.push(5);
+    pstu.push(6);
+while(!pstu.empty()){
+    cout<<pstu.top()<<" ";
+    pstu.pop();
 }
 }
